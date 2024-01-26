9b5a96e96908368212b8add52b7d5f10231c6b20
Vaibhav Varshney <vaibhavvarshney@Vaibhavs-MacBook-Air.local>
2024-01-26 20:25:52 +0530

    removed api folder

--- app/api/post/route.ts
+++
@@ -1,8 +0,0 @@
-import { POST } from "@/data/post";
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function GET(req: NextRequest, res: NextResponse) {
-    return Response.json({
-        posts: POST,
-    });
-}
